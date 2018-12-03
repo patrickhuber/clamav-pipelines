79e87879f1527779f092288cfeebee8effc4b5f5
patrickhuber <patrickhuber@users.noreply.github.com>
2018-12-03 17:41:07 -0500

    adds significant logging

--- tasks/apply-clamav/task.sh
+++ tasks/apply-clamav/task.sh
@@ -28,21 +28,30 @@ fi
 # get the pivnet resource version of clamav    
 target_clamav_version=$(bosh int clamav-addon/metadata.yaml --path /release/version)
 
+echo "target version $target_clamav_version found from pivnet"
+
 # if the named runtime exists
 if bosh runtime-config --name=clamav > bosh_info/clamav-runtime-config; then    
-
+    
     # and get the installed version of clamav
-    installed_clamav_version = $(bosh int bosh_info/runtime-config --path /releases/name=clamav/version)
+    installed_clamav_version=$(bosh int bosh_info/runtime-config --path /releases/name=clamav/version)
+
+    echo "current version $installed_clamav_version installed"
 
     # if versions match, exit
     if ["$installed_clamav_version" -eq "$target_clamav_version"]; then
+        echo "target version $target_clamav_version matches installed version $installed_clamav_version. exiting"
         exit 0
     fi
+
+    echo "version mismatch. upgrading to $target_clamav_version"
 fi
 
+echo "uploading new release of clamav $target_clamav_version to bosh"
 # upload the new version of clamav
 bosh upload-release clamav-addon/clamav-$target_clamav_version.tgz
 
+echo "generating named runtime config"
 # create the interpolated named runtime config 
 # overwrite it if it exists because we don't care what version is installed
 bosh int clamav-pipelines/runtime.yml \
@@ -50,6 +59,7 @@ bosh int clamav-pipelines/runtime.yml \
     -v clamav_version=$target_clamav_version \
     > bosh_info/clamav-runtime-config
 
+echo "updating nameed runtime config"
 # update the clamav runtime config
 bosh --non-interactive update-config \
   --type=runtime \
